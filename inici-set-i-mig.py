c7d79517de1540be34187a3a59250cf9a5d760c8
DavidIbanezMoya <73888235+DavidIbanezMoya@users.noreply.github.com>
2020-12-11 12:56:23 +0100

    Versión actualizada

--- inici-set-i-mig.py
+++ inici-set-i-mig.py
@@ -18,7 +18,7 @@ while flag_salir==False:
             flag_inicio=False
             flag_salir=True
         if opcion<1 or opcion>3:
-            opcion=int(input("El numero es incorrecto, pon uno de los establecidos.\n Opción: "))
+            opcion=int(input("El numero es incorrecto, pon uno de los establecidos.\nOpción: "))
     while flag_jugar==False:
         print("¿A que quieres jugar?\n1)Partida vs bots\n2)Partida multijudaor\n3)Volver al inicio")
         opcion=int(input("Opción: "))
@@ -31,6 +31,8 @@ while flag_salir==False:
         if opcion==3:
             flag_jugar=True
             flag_inicio=True
+        if opcion<1 or opcion>3:
+            opcion=int(input("El numero es incorrecto, pon uno de los establecidos.\nOpción: "))
     while flag_bots==False:
         print("¿Que quieres hacer?\n1)Empezar una partida\n2)Seleccionar modo de juego")
         opcion=int(input("Opción: "))
@@ -39,6 +41,8 @@ while flag_salir==False:
         if opcion==2:
             flag_bots=True
             flag_jugar=False
+        if opcion<1 or opcion>2:
+            opcion=int(input("El numero es incorrecto, pon uno de los establecidos.\nOpción: "))
     while flag_multi==False:
         print("¿Que quieres hacer?\n1)Empezar una partida\n2)Seleccionar modo de juego")
         opcion=int(input("Opción: "))
@@ -47,6 +51,8 @@ while flag_salir==False:
         if opcion==2:
             flag_multi=True
             flag_jugar=False
+        if opcion<1 or opcion>2:
+            opcion=int(input("El numero es incorrecto, pon uno de los establecidos.\nOpción: "))
     #Opciones e información
     while flag_info==False:
         print("¿Que quieres ver?\n1)Reglas\n2)Consultas\n3)Volver al inicio")
@@ -60,8 +66,12 @@ while flag_salir==False:
                 print("El ganador de un turno es el que consigue siete y medio, o el que se\n acerca mas, sin pasarse. Para ganar la partida no se debe quedar sin puntos.")
             if subopcion==3:
                 print()
+            if subopcion<1 or subopcion>3:
+                subopcion=int(input("La opción no es correcta. Pon otra. \nOpción: "))
         if opcion==2:
             print("Aqui se muestran las diferentes consultas de mySQL")
         if opcion==3:
             flag_info=True
             flag_inicio=True
+        if opcion<1 or opcion>3:
+            opcion=int(input("El numero es incorrecto, pon uno de los establecidos.\nOpción: "))
